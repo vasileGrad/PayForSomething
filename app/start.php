380ad2c963c8f0d91e0aecb8d56eb4207dffc597
Grad Vasile <grad_simplu@yahoo.com>
2017-10-07 13:11:21 +0300

    Setting all the variables and connecting with the PayPal account

--- app/start.php
+++ app/start.php
@@ -5,7 +5,7 @@ require 'vendor/autoload.php';
 define('SITE_URL', 'http://localhost:83/payPal/PayForSomething/index.php');
 
 $paypal = new \PayPal\Rest\ApiContext(
-	new \PayPal\Auth\OAuthTokenredential(
+	new \PayPal\Auth\OAuthTokencredential(
 		'AUb99sBdn3KljOoUvno3RXKWcdcu4iWys-w-a8y1xZss1hFaMEFBE2xyeslct0lEGh_LCnDj72JrELvt', // Client Id
 		'EOGhvXA1-DaQPdl2CUAuxzIZyG8TcL8mFTmufddPvF65y5Kwl-NqCLJkdKuKJm5zR23qA_njrmarlxOv'  // Secret
 	)
